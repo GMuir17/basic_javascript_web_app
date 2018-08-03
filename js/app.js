2fa80fe818c5f9b1e6802272db9765d6563e56bc
Gary Muir <garymuir17@gmail.com>
2018-08-03 09:39:44 +0100

    sets up first event listener in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-// keep
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.querySelector("#emperor-form");
+  const emperorList = document.createElement("ul");
+  const emperorListDiv = document.querySelector("#emperor-list");
+  emperorListDiv.appendChild(emperorList);
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+
+
+
+
+
+});
