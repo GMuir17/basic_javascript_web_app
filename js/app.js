7e832f0c3e3460ae27e030cdb2238a44b7b65c0e
Gary Muir <garymuir17@gmail.com>
2018-08-03 14:41:33 +0100

    adds the hidden property to the emperor list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.querySelector("#emperor-form");
+
   const deleteButton = document.querySelector("#delete-button")
+  deleteButton.hidden = true;
+
   const emperorList = document.createElement("ul");
+
   const emperorListDiv = document.querySelector("#emperor-list");
+  emperorListDiv.hidden = true;
+
+  const emperorListTitle = document.querySelector("#emperor_list_title");
+  emperorListTitle.hidden = true;
+
   emperorListDiv.appendChild(emperorList);
+
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    deleteButton.hidden = false;
+    emperorListDiv.hidden = false;
+    emperorListTitle.hidden = false;
 
     const firstName = event.target.first_name.value;
     const familyName = event.target.family_name.value;
